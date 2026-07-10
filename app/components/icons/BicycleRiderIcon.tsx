09d10d9581d91c9497ee1e234420422e5dc899ae
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2026-07-10 18:12:44 +0100

    feat: create BicycleRiderIcon

---
+++ app/components/icons/BicycleRiderIcon.tsx
@@ -0,0 +1,41 @@
+export default function BicycleRiderIcon({ className }: { className: string }) {
+  return (
+    <>
+      <svg
+        className={className}
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M15.4167 17.5C17.0275 17.5 18.3333 16.1942 18.3333 14.5834C18.3333 12.9725 17.0275 11.6667 15.4167 11.6667C13.8058 11.6667 12.5 12.9725 12.5 14.5834C12.5 16.1942 13.8058 17.5 15.4167 17.5Z"
+          stroke="currentColor"
+          strokeWidth="1.66667"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M4.58268 17.5C6.19351 17.5 7.49935 16.1942 7.49935 14.5834C7.49935 12.9725 6.19351 11.6667 4.58268 11.6667C2.97185 11.6667 1.66602 12.9725 1.66602 14.5834C1.66602 16.1942 2.97185 17.5 4.58268 17.5Z"
+          stroke="currentColor"
+          strokeWidth="1.66667"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M12.4993 4.99998C12.9596 4.99998 13.3327 4.62688 13.3327 4.16665C13.3327 3.70641 12.9596 3.33331 12.4993 3.33331C12.0391 3.33331 11.666 3.70641 11.666 4.16665C11.666 4.62688 12.0391 4.99998 12.4993 4.99998Z"
+          stroke="currentColor"
+          strokeWidth="1.66667"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M10 14.5834V11.6667L7.5 9.16669L10.8333 6.66669L12.5 9.16669H14.1667"
+          stroke="currentColor"
+          strokeWidth="1.66667"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </>
+  );
+}
