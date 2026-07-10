ff92d7b8de8043d043eef96b6b46f8126bfdc582
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2026-07-10 18:11:23 +0100

    feat: create HowItWorksSection component

---
+++ app/components/HowItWorksSection/HowItWorksSection.tsx
@@ -0,0 +1,14 @@
+import SendPackageCard from "./SendPackageCard";
+
+export default function HowItWorksSection() {
+  return (
+    <section className="pt-3 px-[22px] bg-white">
+      <h3 className="text-[#5A687C] text-xs font-semibold">HOW IT WORKS</h3>
+      {/* Section Heading */}
+      <h1 className="mt-[15px] mb-[46px] text-[#0B111E] text-5xl font-bold">
+        Two sides. One smooth flow.
+      </h1>
+      <SendPackageCard />
+    </section>
+  );
+}
