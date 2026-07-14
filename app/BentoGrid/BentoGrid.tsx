46fd84f9fea6324b6c7875d2e286c71063f88d8c
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2026-07-14 17:11:31 +0100

    feat: scafold the BentoGrid

---
+++ app/BentoGrid/BentoGrid.tsx
@@ -0,0 +1,9 @@
+import PoweringBusinessSection from "./PoweringBusinessSection";
+
+export default function BentoGrid() {
+  return (
+    <div className="flex items-center p-12 bg-[#FBF9F4] w-full">
+      <PoweringBusinessSection />
+    </div>
+  );
+}
