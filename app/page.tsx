81c1bc4f978144a1f0ee24bed456902a4cc8acea
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2026-07-22 18:12:55 +0100

    chore: comment out some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
-import BentoGrid from "./BentoGrid/BentoGrid";
+import BentoGrid from "./components/BentoGrid/BentoGrid";
 import FeaturesSection from "./components/Features/FeaturesSection";
 import HeroBanner from "./components/HeroBanner/HeroBanner";
 import HowItWorksSection from "./components/HowItWorksSection/HowItWorksSection";
+import UseCasesSection from "./components/UseCasesSection/UseCasesSection";
+import WhyQikmoveSection from "./components/WhyQikmoveSection/WhyQikmoveSection";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
     <div>
       <HeroBanner />
       <HowItWorksSection />
-      <FeaturesSection />
+      {/* <FeaturesSection />
       <BentoGrid />
+      <WhyQikmoveSection />
+      <UseCasesSection />
+      <Footer /> */}
     </div>
   );
 }
