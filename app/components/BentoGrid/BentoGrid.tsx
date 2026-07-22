7a4648c05813ce9e12ad831db6ac66f6cbab5c02
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2026-07-22 14:35:30 +0100

    chore: change BentoGrid location

--- app/components/BentoGrid/BentoGrid.tsx
+++ app/components/BentoGrid/BentoGrid.tsx
@@ -1,9 +1,11 @@
 import PoweringBusinessSection from "./PoweringBusinessSection";
+import SimpleDeliverySection from "./SimpleDeliverySection";
 
 export default function BentoGrid() {
   return (
-    <div className="flex items-center p-12 bg-[#FBF9F4] w-full">
+    <div className="flex items-center gap-x-3 p-12 bg-[#FBF9F4] w-full">
       <PoweringBusinessSection />
+      <SimpleDeliverySection />
     </div>
   );
 }
