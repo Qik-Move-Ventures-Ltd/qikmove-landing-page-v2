3afc85e7459544b202d4065299e177a300ede4f7
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2026-09-14 23:26:51 +0100

    chore: move some classes to a new line

--- app/components/NavHeader/LinkItem.tsx
+++ app/components/NavHeader/LinkItem.tsx
@@ -10,7 +10,8 @@ export default function LinkItem({
   return (
     <Link
       href={link}
-      className="font-medium text-lg min-[492px]:text-xl min-[960px]:text-base text-[#5A687C] hover:text-[#3838C557]"
+      className="font-medium text-lg min-[492px]:text-xl min-[960px]:text-base text-[#5A687C] 
+      hover:text-[#3838C557]"
     >
       {navItemTitle}
     </Link>
